0125fba3fb56bcc17cedf15e0139991fe63d9153
Wonshik KIM <wonshikkim.kr@gmail.com>
2024-03-06 09:40:21 +0900

    fix: disable tracing in TraceService with NoSpan annotation

--- src/trace.service.ts
+++ src/trace.service.ts
@@ -1,10 +1,9 @@
 import { Injectable } from '@nestjs/common';
 import { ContextManager } from 'skywalking-backend-js';
+import { NoSpan } from './span.decorator';
 
 @Injectable()
-/**
- * Service for managing traces and spans.
- */
+@NoSpan()
 export class TraceService {
   /**
    * Gets the current span.
